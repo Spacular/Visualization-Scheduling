376ab709c93c020ca9da0dac942f29c2abe7f147
Zerglrisk <wmf1235@naver.com>
2015-05-19 12:14:56 +0900

    Revert "Revert "Init""
    This reverts commit 54431ad14408c098ab5a8c1bc73834e06f2bb03d.

---
+++ VisualizationScheduling/VisualizationScheduling/Sub_From.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace VisualizationScheduling
+{
+    public partial class Sub_From : Form
+    {
+        public Sub_From()
+        {
+            InitializeComponent();
+        }
+    }
+}
