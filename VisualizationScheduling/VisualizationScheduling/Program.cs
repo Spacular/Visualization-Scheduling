3f6f4621f555bd6de729d88a401a5f42525aa63e
Zerglrisk <wmf1235@naver.com>
2015-05-21 08:31:38 +0900

    UI변경 및 Process 클래스 추가
    UI변경 및 Process 클래스 추가

--- VisualizationScheduling/VisualizationScheduling/Program.cs
+++ VisualizationScheduling/VisualizationScheduling/Program.cs
@@ -16,7 +16,7 @@ namespace VisualizationScheduling
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Main_From());
+            Application.Run(new Visualization_Scheduling());
         }
     }
 }
