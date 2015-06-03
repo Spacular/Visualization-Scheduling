0d9ed49759ef0ce60414d62c2646c0eb03147062
Zerglrisk <wmf1235@naver.com>
2015-06-04 03:39:02 +0900

    Priority 스케쥴링
    추가까지는 아님

--- VisualizationScheduling/VisualizationScheduling/Priority.cs
+++ VisualizationScheduling/VisualizationScheduling/Priority.cs
@@ -8,6 +8,67 @@ namespace VisualizationScheduling
 {
     class Priority
     {
+       
+        public static List<Result> Run(List<Process> jobList, List<Result> resultList)
+        {
+            int currentProcess = 0;
+            int cpuTime = 0;
+            int cpuDone = 0;
+            int runTime = 0;
 
+            List<Result> readyQueue = new List<Result>();
+            for (int i = 0; i < jobList.Count; i++)
+            {
+                for (int j = i + 1; j < jobList.Count; j++)
+                    if (jobList.ElementAt(i).ArriveTime == jobList.ElementAt(j).ArriveTime)
+                    {
+                        jobList.ElementAt(j).ArriveTime++;
+                        jobList.ElementAt(j).same++;
+                    }
+            }
+            do
+            {
+                if (jobList.Count != 0)
+                {
+                    Process frontJob = jobList.ElementAt(0);
+                    if (frontJob.ArriveTime == runTime)
+                    {
+                        readyQueue.Add(new Result(frontJob.ProcessID, frontJob.BurstTime, 0, frontJob.same, frontJob.Priority));
+                        jobList.RemoveAt(0);
+                    }
+                }
+                if (currentProcess == 0)
+                {
+
+                    if (readyQueue.Count != 0)
+                    {
+
+                        Result rq = readyQueue.ElementAt(0);
+                        resultList.Add(new Result(rq.processID, runTime, rq.waitingTime, rq.burstTime, rq.Priority));
+                        cpuDone = rq.burstTime;
+                        cpuTime = 0;
+                        currentProcess = rq.processID;
+                        readyQueue.RemoveAt(0);
+                    }
+                }
+                else
+                {
+                    if (cpuTime == cpuDone)
+                    {
+                        currentProcess = 0;
+                        continue;
+                    }
+                }
+                cpuTime++;
+                runTime++;
+                for (int i = 0; i < readyQueue.Count; i++)
+                {
+                    readyQueue.ElementAt(i).waitingTime++;
+                }
+
+            } while (jobList.Count != 0 || readyQueue.Count != 0 || currentProcess != 0);
+
+            return resultList;
+        }
     }
 }
